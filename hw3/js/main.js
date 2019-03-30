9b8cfa536bb352af4b4b3ab2f41b992477cc257a
ValeraBahleiUA <vbaglej@gmail.com>
2019-03-30 22:52:30 +0200

    TheVeryFirstTask

---
+++ hw3/js/main.js
@@ -0,0 +1,14 @@
+
+let a = "block"; //Записать в виде switch case следующее условие:
+switch(a) {
+    case 'block':
+        console.log('block');
+        break;
+    case 'none':
+        console.log('none');
+        break;
+    case 'inline':
+        console.log('inline');
+        break;
+    default: console.log("other");
+}
