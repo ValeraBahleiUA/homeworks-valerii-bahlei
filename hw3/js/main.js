a22d348e89c7f260a30c94a3055549f624a13d26
ValeraBahleiUA <vbaglej@gmail.com>
2019-03-31 02:13:32 +0200

    StillNotDone

--- hw3/js/main.js
+++ hw3/js/main.js
@@ -13,8 +13,7 @@ switch(a) {
     default: console.log("other");
 }
 
-let variable1 = "hidden"; //Из задач по условному оператору if else выполнить задачи 1, 2 и 3 в виде тернарного
-оператора.
+let variable1 = "hidden"; //Из задач по условному оператору if else выполнить задачи 1, 2 и 3 в виде тернарного оператора
 variable1 == "hidden" ? variable1 = "visible" :
 variable1 = "hidden";
 
@@ -27,3 +26,13 @@ alert("variable 2 is NaN")
 let car = { name: 'Lexus', age: 10, create: 2008, needRepair: false }
 car.age > 5 ? car.needRepair = true :
 car.needRepair = false, alert("your car is not broken");
+
+var strokeVarInput = "i am in the easycode";
+var strokeVarOutput = '';
+for (var i = 0; i < strokeVarInput.length; i++) {
+  strokeVarOutput += (strokeVarInput[i - 1] == ' ') ? strokeVarInput[i].toUpperCase() : strokeVarInput[i];
+
+}
+strokeVarOutput.charAt(0).toUpperCase()
+alert(strokeVarOutput);
+
